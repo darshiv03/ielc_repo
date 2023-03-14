3a2573df52dcff5a4c281b64e9dc79c9a4106286
Samantha Iraheta <sairaheta1@gmail.com>
2023-03-14 02:39:14 -0700

    finished toggle dropdown

--- src/Components/JS/DropDown.js
+++ src/Components/JS/DropDown.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import "../CSS/DropDown.css"
 import programInfo from '../../data/programInfo';
+import { useState } from 'react';
 
-export default function DropDown() {
+export default function DropDown({dropdown}) {
 
     return (
         <div>
-            <ul className="dropdown">
+            <ul className ={`dropdown ${dropdown ? "show" : ""}`}>
                 {programInfo.map((programInfo, index) => (
                     <li key={index} className="menu-items">
                         <a href ={programInfo.url}>Grade {programInfo.grade}</a>
--- src/Components/JS/Navbar.js
+++ src/Components/JS/Navbar.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import "../CSS/Navbar.css"
 import DropDown from './DropDown';
+import { useState } from 'react';
 
 export default function NavBar() {
+  const [dropdown, setDropdown] = useState(false);
 
     return (
         <div>
-          <div className='program-tester'>Programs
-          <DropDown></DropDown>
-          </div>
+          <button className='program-tester'
+          type="button" aria-haspopup="menu"
+          aria-expanded={dropdown ? "true" : "false"}
+          onClick={() => setDropdown((prev) => !prev)}
+          >
+            Programs
+          </button>
+          <DropDown dropdown={dropdown}></DropDown>
         </div>
     )
 }
