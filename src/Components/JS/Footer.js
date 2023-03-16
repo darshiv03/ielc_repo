080af34e2c79543296a15535d014a366b17f445e
janania <janani.asokumar@gmail.com>
2023-03-15 17:58:40 -0700

    Edited footer
    Edited footer to adjust spacing and links

--- src/Components/JS/Footer.js
+++ src/Components/JS/Footer.js
@@ -4,38 +4,37 @@ import LogoIcon from "./LogoIcon.js";
 import MailIcon from "./MailIcon.js";
 import InstaIcon from "./InstaIcon.js";
 import FacebookIcon from "./FacebookIcon.js";
-
+import {Link} from 'react-router-dom';
 
 export default function Footer() {
 
     return (
         <div class = "bod">
             
-            <div class = "frame">
             <div class ="logo">
                 {LogoIcon(50)}
             </div>
-                <div class = "p1">
-                    <p class = "a">Learn More</p>
-                         <p>About us <br></br>
-                        Register A Student<br></br>
-                        Become a Tutor <br></br>
-                        Donate</p>
+                <div className='learn-more-container'>
+                    <p className = "a">Learn More</p>
+                    <Link to="/about-us">About us</Link> 
+                    <Link to ="/register">Register A Student</Link>
+                    <Link to = "/">Become a Tutor</Link>
+                    <Link to = "/">Donate</Link>
                 </div>
                 
-                <div class = "p2">
-                    <p class = "a">Programs</p>
-                    <p>Grade 1 <br></br>
-                        Grade 2<br></br>
-                        Grade 3 <br></br>
-                        Grade 4 <br></br>
-                        Grade 5 <br></br></p>
+                <div className = "program-container">
+                    <p className = "a">Programs</p>
+                    <Link to="/program/1"> Grade 1</Link>
+                    <Link to="/program/2"> Grade 2</Link>
+                    <Link to="/program/3"> Grade 3</Link>
+                    <Link to="/program/4"> Grade 4</Link>
+                    <Link to="/program/5"> Grade 5</Link>
                 </div>
 
                 <div class ="icons">
-                    {MailIcon(50)}
-                    {InstaIcon(50)}
-                    {FacebookIcon(50)}
+                    <a>{MailIcon(50)}</a>
+                    <a>{InstaIcon(50)}</a>
+                    <a>{FacebookIcon(50)}</a>
                 </div> 
 
                 <div class = "footer">
@@ -46,7 +45,6 @@ export default function Footer() {
                 </div>
 
                 
-            </div>
         </div>
     )
 }
