80e4f9faf25dc7c5e65b092444a0821b151e6f1a
ylylime <eming@ucdavis.edu>
2023-03-15 23:40:34 -0700

    fixed styling

--- src/Pages/JS/ContactUs.js
+++ src/Pages/JS/ContactUs.js
@@ -3,29 +3,29 @@ import "../CSS/ContactUs.css"
 
 
 export default function ContactUs() {
-    return(
-        <div>
-            <h1>Contact Us</h1>
-            <h3>Based in Davis, California</h3>
-            <h4>Get in touch with our team directly at elementarylearningcenter@gmail.com or fill out the form below.
-                We'll get back to you as soon as possible!
-            </h4>
-            <form action="/action_page.php">
-                <label for="fname">First Name</label><br>
-                <input type="text" id="fname" name="fname" required minlength="1" placeholder="Type here" ><br>
-                <label for="lname">Last Name</label><br>
-                <input type="text" id="lname" name="lname" required minlength="1" placeholder="Type here"><br>
-                <label for="mail">Email Address</label><br>
-                <input type="email" id="mail" name="mail" placeholder="Type here"><br>
-                <label for="phone">Phone Number</label><br>
-                <input type="tel" id="phone" name="phone" placeholder="Type here"><br>
-                <label for="msg">Message</label><br>
-                <input type="text" id="msg" name="msg" placeholder="Type here"><br><br>
+    return (
+        <div className= "contact-us-container">
+            <h1 class="contact-header">Contact Us</h1>
+            <h3 class="based">Based in Davis, California</h3> 
+            <p class="get-in-touch">Get in touch with our team directly at <span class = "red-letters">elementarylearningcenter@gmail.com</span> or fill out the form below.
+            <br></br><br></br>
+                We'll get back to you as soon as possible!</p> <br></br><br></br>
+            <h3 class= "red-letters">* = required</h3><br></br>
+
+            <form>
+                <label for="fname">First Name *</label>
+                <input type="text" id="fname" name="fname" required minlength="1" placeholder="Type here"/>
+                <label for="lname">Last Name *</label>
+                <input type="text" id="lname" name="lname" required minlength="1" placeholder="Type here"/>
+                <label for="mail">Email Address *</label>
+                <input type="email" id="mail" name="mail" placeholder="Type here"/>
+                <label for="phone">Phone Number</label>
+                <input type="tel" id="phone" name="phone" placeholder="Type here"/>
+                <label for="msg">Message</label>
+                <textarea type="text" id="msg" name="msg" placeholder="Type here"/>
                 
-                <div>
-                    <button>Send Message</button>
-                    <input type="submit" value="Submit">
-                </div>
+                <input type="submit" value="Send Message"/>
+                
             </form>
         </div>
     )
