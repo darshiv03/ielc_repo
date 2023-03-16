2c8e4c7576d409edb49a2d5dd21beae6f932df5c
Brendan <brendansleung@gmail.com>
2023-03-16 15:43:12 -0700

    iniital commit

--- src/Pages/JS/Program.js
+++ src/Pages/JS/Program.js
@@ -12,6 +12,37 @@ export default function Program() {
     const data = programInfo[gradeLevel - 1];
 
     return (
-        <div>{JSON.stringify(data)}</div>
+        <div>
+            <div class="image-1">
+                <h1>Our Summer <br></br>
+                    Program 2023</h1>
+                <p>7 weeks • 2 Hours per Week</p>
+            </div>
+
+            <div class="text">
+                <h3>Designed to prepare & reinforce literacy concepts.</h3>
+                <p>Our tutors prepare students from grades 1 to 5 with reading comprehension and grammar skills in accordance with the Common Core Standards in 60-minute sessions conducted over Zoom.</p>
+                {/* button goes here */}
+                <h3 class="red">Starts June 19, 2023</h3>
+            </div>
+
+            <div class="grade-finder">
+                <img src="/grade-finder-icon.png"></img>
+                <h1>Find Your Grade</h1>
+                {/* grade buttons below */}
+                <p class="button-text">Grade 1
+                <br></br>Grade 2
+                <br></br>Grade 3
+                <br></br>Grade 4
+                <br></br>Grade 5</p>
+            </div>
+
+            <div class="image-2">
+                <h3>Our Interactive<br></br>
+                    Learning Culture</h3>
+                <p>Through our 7-week program, we encourage curiosity, engagement, and academic excellence. We create an active learning environment through our interactive activities. We are dedicated to creating a supportive and inclusive environment where students can thrive and unlock their full potential.</p>
+            </div>
+            <div>{JSON.stringify(data)}</div>
+        </div>
     )
 }
