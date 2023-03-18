6ac5f3206e1440fbd73efc44ead0978bfe3b79fd
Austin Shih <austinhshih@gmail.com>
2023-03-17 20:13:35 -0700

    Fixed react warnings

--- src/Pages/JS/ContactUs.js
+++ src/Pages/JS/ContactUs.js
@@ -5,23 +5,23 @@ import "../CSS/ContactUs.css"
 export default function ContactUs() {
     return (
         <div className= "contact-us-container">
-            <h1 class="contact-header">Contact Us</h1>
-            <h3 class="based">Based in Davis, California</h3> 
-            <p class="get-in-touch">Get in touch with our team directly at <span class = "red-letters">elementarylearningcenter@gmail.com</span> or fill out the form below.
+            <h1 className="contact-header">Contact Us</h1>
+            <h3 className="based">Based in Davis, California</h3> 
+            <p className="get-in-touch">Get in touch with our team directly at <span className = "red-letters">elementarylearningcenter@gmail.com</span> or fill out the form below.
             <br></br><br></br>
                 We'll get back to you as soon as possible!</p> <br></br><br></br><br></br>
-            <h3 class= "red-letters">* = required</h3>
+            <h3 className= "red-letters">* = required</h3>
 
             <form>
-                <label class= "required" for="fname">First Name <span> </span></label><br></br>
-                <input type="text" id="fname" name="fname" required minlength="1" placeholder="Type here"/><br></br>
-                <label class= "required" for="lname">Last Name </label><br></br>
-                <input type="text" id="lname" name="lname" required minlength="1" placeholder="Type here"/><br></br>
-                <label class= "required" for="mail">Email Address </label><br></br>                    <input type="email" id="mail" name="mail" placeholder="Type here"/><br></br>
-
-                <label for="phone">Phone Number</label><br></br>
+                <label className= "required" htmlFor="fname">First Name <span> </span></label><br></br>
+                <input type="text" id="fname" name="fname" required minLength="1" placeholder="Type here"/><br></br>
+                <label className= "required" htmlFor="lname">Last Name </label><br></br>
+                <input type="text" id="lname" name="lname" required minLength="1" placeholder="Type here"/><br></br>
+                <label className= "required" htmlFor="mail">Email Address </label><br></br>
+                <input type="email" id="mail" name="mail" placeholder="Type here"/><br></br>
+                <label htmlFor="phone">Phone Number</label><br></br>
                 <input type="tel" id="phone" name="phone" placeholder="Type here"/><br></br>
-                <label for="msg">Message</label><br></br>
+                <label htmlFor="msg">Message</label><br></br>
                 <textarea type="text" id="msg" name="msg" placeholder="Type here"/><br></br>
                 
                 <input type="submit" value="Send Message" />
