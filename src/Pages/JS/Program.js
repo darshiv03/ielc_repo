56b86c0305c6520c1c2f73aeee3c42867f8366ad
Samantha Iraheta <sairaheta1@gmail.com>
2023-04-01 15:32:52 -0700

    split page into four sections

--- src/Pages/JS/Program.js
+++ src/Pages/JS/Program.js
@@ -24,20 +24,28 @@ export default function Program() {
         <div>
             <img src={student_img} alt="Student raising their hand in classroom" class="image"/>
             <div class="page-content">
-                <h1>Grade {gradeLevel}</h1>
-                <h3>{data.mainHeading}</h3>
-                <p class="p-medium">{data.description}</p>
-                <h2 class="goals-heading">{data.subHeading}</h2>
-                <ul class="p-large">{goals}</ul>
-                <div class="tutor-header">
-                    <div class="portrait"><img src={tutor_img} alt="Portrait of tutor Jane Rose"/></div>
-                    <h3>Meet {data.tutorName}</h3>
-                    <h4>{data.tutorSchool}</h4>
-                </div>
-                <p class="p-medium">{data.tutorDescr}</p>
-                <h3>{data.slogan}</h3>
-                <button>{data.register_bttn}</button>
-                <p class="deadline">{data.deadline}</p>
+                <section class="program-header">
+                    <h1>Grade {gradeLevel}</h1>
+                    <h3>{data.mainHeading}</h3>
+                    <p class="p-medium">{data.description}</p>
+                </section>
+                <section class="goals">
+                    <h2 class="goals-heading">{data.subHeading}</h2>
+                    <ul class="p-large">{goals}</ul>
+                </section>
+                <section class="tutor-section">
+                    <div class="tutor-header">
+                        <div class="portrait"><img src={tutor_img} alt="Portrait of tutor Jane Rose"/></div>
+                        <h3>Meet {data.tutorName}</h3>
+                        <h4>{data.tutorSchool}</h4>
+                    </div>
+                    <p class="p-medium">{data.tutorDescr}</p>
+                </section>
+                <section class="registration">
+                    <h3>{data.slogan}</h3>
+                    <button>{data.register_bttn}</button>
+                    <p class="deadline">{data.deadline}</p>
+                </section>
                 </div>
         </div>
     )
