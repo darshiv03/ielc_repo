348b1519fb69864aace685338df7f276677585c5
Austin Shih <austinhshih@gmail.com>
2023-04-10 21:46:47 -0700

    small fixes

--- api/contact.js
+++ api/contact.js
@@ -1,56 +1,53 @@
-import axios from "axios"
 import nodemailer from "nodemailer"
 
 export default async function contactMailer(req, res){
     if (req.method !== 'POST'){
         res.render(contact.html) //front-end html page file
     } else {
-        {
-            /*const transporter = nodemailer.createTransport({
-                service: "gmail",
-                auth: {
-                  user: process.env.GMAIL_USER,
-                  pass: process.env.PASSWORD,
-                }
-            })*/
-
-            const transporter= nodemailer.createTransport({
-                host: 'smtp.ethereal.email',
-                port: 587,
-                auth: {
-                    user: 'torey95@ethereal.email',
-                    pass: 'M8FRfgbtshjxjUMcZt'
-                }
-            })
-            const data = JSON.parse(req.body)
-
-            const firstname= data.fname
-            const lastname= data.lname
-            const email= data.mail
-            const question= data.msg;
-            
-            const mailOption = {
-                from: "senderemail@gmail.com",
-                to: "recipientemail@gmail.com",
-                subject: 'You have a message!',
-                html: `You have a message from 
-                Name: ${firstname} ${lastname}
-                Email : ${email}
-                Question: ${question}`,
+        /*const transporter = nodemailer.createTransport({
+            service: "gmail",
+            auth: {
+                user: process.env.GMAIL_USER,
+                pass: process.env.PASSWORD,
             }
-              
-            transporter.sendMail(mailOption, function (error, info){
-                if (error) {
-                  console.log(error)
-                  res.send({
-                    message: "Couldn't send email; try again in a few moments.",
-                  })
-                  return
-                } else {
-                  console.log("sent " + info.response)
-                  res.send({ message: "Email sent!" });
-                }
-            });
+        })*/
+
+        const transporter= nodemailer.createTransport({
+            host: 'smtp.ethereal.email',
+            port: 587,
+            auth: {
+                user: 'torey95@ethereal.email',
+                pass: 'M8FRfgbtshjxjUMcZt'
+            }
+        })
+        const data = JSON.parse(req.body)
+
+        const firstname= data.fname
+        const lastname= data.lname
+        const email= data.mail
+        const question= data.msg;
+        
+        const mailOption = {
+            from: "senderemail@gmail.com",
+            to: "recipientemail@gmail.com",
+            subject: 'You have a message!',
+            html: `You have a message from 
+            Name: ${firstname} ${lastname}
+            Email : ${email}
+            Question: ${question}`,
         }
+            
+        transporter.sendMail(mailOption, function (error, info){
+            if (error) {
+                console.log(error)
+                res.send({
+                message: "Couldn't send email; try again in a few moments.",
+                })
+                return
+            } else {
+                console.log("sent " + info.response)
+                res.send({ message: "Email sent!" });
+            }
+        });
     }
 }
