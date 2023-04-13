1e066ecdb038e7236c05f84cc1a87e390e449f7f
yugii28 <yugandhar28@hotmail.com>
2023-04-12 18:48:34 -0700

    fixed h1 conflicts

--- src/Components/JS/OurSummerProgramTop.js
+++ src/Components/JS/OurSummerProgramTop.js
@@ -19,26 +19,25 @@ export default function Program() {
                     Program 2023</h1>
                 <p class="image-subtext">7 weeks • 2 Hours per Week</p>
             </div>
-
-            <div class="text">
-                <div class="column">
-                    <h3>Designed to prepare & reinforce literacy concepts.</h3>
-                    <p>Our tutors prepare students from grades 1 to 5 with reading comprehension and grammar skills in accordance with the Common Core Standards in 60-minute sessions conducted over Zoom.</p>
+            <div class = "our-summer-page-top-text-container">
+                <div class="text">
+                    <div class="column">
+                        <h3>Designed to prepare & reinforce literacy concepts.</h3>
+                        <p>Our tutors prepare students from grades 1 to 5 with reading comprehension and grammar skills in accordance with the Common Core Standards in 60-minute sessions conducted over Zoom.</p>
+                    </div>
+                    <div class="column">
+                        <h3>Our Interactive
+                            Learning Culture</h3>
+                        <p>Through our 7-week program, we encourage curiosity, engagement, and academic excellence. We create an active learning environment through our interactive activities. We are dedicated to creating a supportive and inclusive environment where students can thrive and unlock their full potential.</p>
+                    </div>
                 </div>
-                <div class="column">
-                    <h3>Our Interactive
-                        Learning Culture</h3>
-                    <p>Through our 7-week program, we encourage curiosity, engagement, and academic excellence. We create an active learning environment through our interactive activities. We are dedicated to creating a supportive and inclusive environment where students can thrive and unlock their full potential.</p>
+                <div class="save-spot">
+                    <div class="button-wrapper">
+                        <button>Save your Free Spot</button>
+                    </div>
+                    <h3 class="red">Starts June 19, 2023</h3>
                 </div>
             </div>
-
-            <div class="save-spot">
-                <div class="button-wrapper">
-                    <button>Save your Free Spot</button>
-                </div>
-                <h3 class="red">Starts June 19, 2023</h3>
-            </div>
-
             <div class="grade-finder">
                 <img src="/grade-finder-icon.png"></img>
                 <h1>Find Your Grade</h1>
@@ -50,14 +49,12 @@ export default function Program() {
                     <Buttons type = "grade5">Grade 5</Buttons>
                 </div>
             </div>
-
-            <div class="image-2">
-                <img src="/programs2.png"></img>
+            <img src="/programs2.png" className = "image-2"></img>
+            <div class="our-summer-page-bottom-text-container">
                 <h3>Our Interactive<br></br>
                     Learning Culture</h3>
                 <p>Through our 7-week program, we encourage curiosity, engagement, and academic excellence. We create an active learning environment through our interactive activities. We are dedicated to creating a supportive and inclusive environment where students can thrive and unlock their full potential.</p>
             </div>
-            <div>{JSON.stringify(data)}</div>
-        </div>
+            </div>
     )
 }
